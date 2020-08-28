d1bc57c36d88b564d663cd0bb3ee00ae781f2ec8
Saurav M. Hiremath <sauravhiremath@gmail.com>
2020-08-28 19:55:48 +0530

    update: add shadow-drop for links

--- src/components/achievementCard/AchievementCard.js
+++ src/components/achievementCard/AchievementCard.js
@@ -1,30 +1,21 @@
-import "./AchievementCard.css";
-import React from "react";
+import './AchievementCard.css';
+import React from 'react';
 import { openUrlInNewTab } from '../helper';
-
 
 const AchievementCard = ({ cardInfo, isDark }) => {
   return (
-    <div className={isDark ? "dark-mode certificate-card" : "certificate-card"}>
+    <div className={isDark ? 'dark-mode certificate-card' : 'certificate-card'}>
       <div className="certificate-image-div">
         <img src={cardInfo.image} alt="achievement" className="card-image" />
       </div>
       <div className="certificate-detail-div">
-        <h5 className={isDark ? "dark-mode card-title" : "card-title"}>
-          {cardInfo.title}
-        </h5>
-        <p className={isDark ? "dark-mode card-subtitle" : "card-subtitle"}>
-          {cardInfo.description}
-        </p>
+        <h5 className={isDark ? 'dark-mode card-title' : 'card-title'}>{cardInfo.title}</h5>
+        <p className={isDark ? 'dark-mode card-subtitle' : 'card-subtitle'}>{cardInfo.description}</p>
       </div>
       <div className="certificate-card-footer">
         {cardInfo.footer.map((v, i) => {
           return (
-            <p
-              key={v.name}
-              className={isDark ? "dark-mode" : null}
-              onClick={() => openUrlInNewTab(v.url)}
-            >
+            <p key={v.name} className={isDark ? 'dark-mode' : null} onClick={() => openUrlInNewTab(v.url)}>
               {v.name}
             </p>
           );
