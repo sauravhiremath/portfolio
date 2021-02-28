1ef463e07227f030d6c16347d62e09ef9505a87d
Saurav M. Hiremath <sauravhiremath@gmail.com>
2021-02-28 22:31:56 +0530

    update: add blogs link as header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,16 +1,16 @@
-import "./Header.css";
-import React, { useContext } from "react";
-import Headroom from "react-headroom";
-import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
-import StyleContext from "../../contexts/StyleContext";
-import { greeting, workExperiences } from "../../portfolio";
+import './Header.css';
+import React, { useContext } from 'react';
+import Headroom from 'react-headroom';
+import ToggleSwitch from '../ToggleSwitch/ToggleSwitch';
+import StyleContext from '../../contexts/StyleContext';
+import { greeting, workExperiences } from '../../portfolio';
 
 const Header = () => {
   const { isDark } = useContext(StyleContext);
-  const exp = workExperiences.viewExperiences;
+  const showExperience = workExperiences.viewExperiences;
   return (
     <Headroom>
-      <header className={isDark ? "dark-menu header" : "header"}>
+      <header className={isDark ? 'dark-menu header' : 'header'}>
         <a href="/" className="logo">
           <span className="grey-color"> &lt;</span>
           <span className="logo-name">{greeting.username}</span>
@@ -20,14 +20,22 @@ const Header = () => {
         <label className="menu-icon" htmlFor="menu-btn">
           <span className="navicon" />
         </label>
-        <ul className={isDark ? "dark-menu menu" : "menu"}>
+        <ul className={isDark ? 'dark-menu menu' : 'menu'}>
+          <li className="blogs">
+            <a href="https://blog.sauravmh.com" target="__blank">
+              <span role="img" aria-label="new">
+                ✨
+              </span>
+              <b>(NEW) View BLOGS</b>
+            </a>
+          </li>
           <li>
             <a href="#skills">Skills</a>
           </li>
           <li>
             <a href="#opensource">Open Source</a>
           </li>
-          {exp === true && (
+          {showExperience === true && (
             <li>
               <a href="#experience">Work Experiences</a>
             </li>
