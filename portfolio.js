258cc00c0061640d80a89440b4ab4e7fa55e1d41
Saurav M Hiremath <sauravhiremath@gmail.com>
2022-07-09 03:12:37 +0530

    feat: add locales

---
+++ portfolio.js
@@ -0,0 +1,24 @@
+import emoji from "react-easy-emoji";
+import en from "locale/en.json";
+
+const current_locale = en
+
+export const about = {
+    display: true,
+    name: current_locale.about.name,
+    title: current_locale.about.title,
+    description: emoji(current_locale.about.description),
+    subheadings: current_locale.about.headers
+};
+
+export const workExperience = {
+    title: current_locale.work_experience.title,
+    experiences: current_locale.work_experience.experiences,
+}
+
+export const technologies = {
+    heading: current_locale.technologies.heading,
+    frameworks: current_locale.technologies.frameworks
+}
+
+export const { testimonials, journey, blogs, contact } = current_locale
