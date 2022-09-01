d736dfa8bac77aab7cecf6b1c46ff470db5599d9
Saurav M Hiremath <sauravhiremath@gmail.com>
2022-09-01 22:41:21 +0530

    fix: ignoreDuringBuilds

--- next.config.js
+++ next.config.js
@@ -4,5 +4,9 @@ module.exports = {
   images: {
     domains: ['res.cloudinary.com'],
   },
-  ignoreDuringBuilds: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
